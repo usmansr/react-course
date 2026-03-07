3e1a27c454934f86762f18a81998f0f2411bea3a
Usman Sarwar <sarwarusman708@gmail.com>
2026-03-07 20:11:11 +0500

    App - 2 completed

--- App - 2/vite-project/src/assets/main.jsx
+++ App - 2/vite-project/src/assets/main.jsx
@@ -1,6 +1,6 @@
 export default function Main() {
   return (
-    <>
+    <main>
       <h1>Fun Facts about react</h1>
       <ul className="facts-list">
         <li>Was first released in 2013</li>
@@ -9,7 +9,6 @@ export default function Main() {
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-
-    </>
+    </main>
   );
 }
--- App - 2/vite-project/src/assets/navbar.jsx
+++ App - 2/vite-project/src/assets/navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     return (
     <header>
         <nav>
-            <img className="navbar-image" src="../react-logo.png" alt="React Logo " />
+            <img className="navbar-image" src="./public/react-logo.png" alt="React Logo " />
             <span>React Facts</span>
         </nav>
     </header>
