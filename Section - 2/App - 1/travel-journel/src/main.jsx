54fd1ead5d4d7d4d682b5bccef53079b3b0f8a34
Usman Sarwar <sarwarusman708@gmail.com>
2026-03-10 19:27:01 +0500

    Props practice

--- Section - 2/App - 1/travel-journel/src/main.jsx
+++ Section - 2/App - 1/travel-journel/src/main.jsx
@@ -1,8 +1,24 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
+
+function App_2() {
+    let firstName = "John";
+    let lastName = "Shmoe"
+    
+    return (
+        <>
+            <h1>Hello, {firstName + " " + lastName} </h1>
+            <h1>It is currently { new Date().getHours() % 12} : { new Date().getMinutes()}</h1>
+        </>
+        
+    )
+}
 
 root.render(
+  <>
     <App />
+    <App_2 />
+  </>
 );
