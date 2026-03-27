670ec0c86fbe5158bff769d5cc2083e0d25730d5
Usman Sarwar <sarwarusman708@gmail.com>
2026-03-27 17:42:14 +0500

    Section 5 Done!

--- Section - 5/vite-project/src/App.jsx
+++ Section - 5/vite-project/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Die from "./Die";
-import { nanoid } from 'nanoid'
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 function App() {
   function generateRandomNewDice() {
@@ -19,31 +20,66 @@ function App() {
     // return newDice;
   }
 
-  const [dice, setDice] = useState(generateRandomNewDice);
-  const diceElements = dice.map(dieObj => (
-      <Die 
-        key={dieObj.id} 
-        value={dieObj.value} 
-        isHeld={dieObj.isHeld} 
-        hold={() => hold(dieObj.id)}
-      />
+  const [dice, setDice] = useState(() => generateRandomNewDice());
+  const buttonRef = useRef(null); //save values bw render cycle without triggering a re-render -> DOM node returned as markup
+
+  const diceElements = dice.map((dieObj) => (
+    <Die
+      key={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={() => hold(dieObj.id)}
+    />
   ));
 
   function rollDice() {
-    setDice(generateRandomNewDice());
+    if (!gameWon) {
+      setDice((oldDice) => {
+        return oldDice.map((die) => {
+          return die.isHeld
+            ? die
+            : { ...die, value: Math.ceil(Math.random() * 6) };
+        });
+      });
+    } else {
+      setDice(generateRandomNewDice());
+    }
   }
 
   function hold(id) {
-    console.log(id);
-
+    // console.log(id);
+    setDice((oldDice) => {
+      return oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      });
+    });
   }
+
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
+
+    useEffect(() => {
+      buttonRef.current.focus()
+    } , [gameWon])
 
   return (
     <main>
+      {gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && (
+          <p>Congratulations, you won! Press "New Game" to start again</p>
+        )}
+      </div>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
 
-      <button className="roll-dice" onClick={rollDice}>
-        Roll Dice
+      <button className="roll-dice" onClick={rollDice} ref={buttonRef}>
+        {gameWon ? "New Game" : "Roll"}
       </button>
     </main>
   );
--- Section - 5/vite-project/src/Die.jsx
+++ Section - 5/vite-project/src/Die.jsx
@@ -1,11 +1,15 @@
 export default function Die(props) {
-    const styles = {
-        backgroundColor : props.isHeld ? "#59E391" : "white"
-    }
-    return (
-        <button 
-            style={styles} 
-            onClick={props.hold}    
-        >{props.value}</button>
-    )
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+  return (
+    <button
+      style={styles}
+      onClick={props.hold}
+      aria-label={`Die with a value of ${props.value} that is ${props.isHeld ? "held" : "not held"}`}
+      aria-pressed={props.isHeld}
+    >
+      {props.value}
+    </button>
+  );
 }
